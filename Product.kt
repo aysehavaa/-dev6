6a232287f644c1ba2a4434181f835b766eb34f09
ayşe hava <157634740+aysehavaa@users.noreply.github.com>
2025-05-16 00:22:48 +0300

    Product.kt

---
+++ Product.kt
@@ -0,0 +1,9 @@
+package com.example.aysikstore.models
+
+data class Product(
+    val name: String,
+    val description: String,
+    val price: Double,
+    val rating: Float,
+    val imageResId: Int
+) 
